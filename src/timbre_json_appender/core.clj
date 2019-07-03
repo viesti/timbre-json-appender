79ec96a6cdc085c244528e38d99800f6f1b96763
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2019-07-03 13:18:01 +0300

    Initial commit

---
+++ src/timbre_json_appender/core.clj
@@ -0,0 +1,65 @@
+(ns timbre-json-appender.core
+  (:require [jsonista.core :as json]
+            [taoensso.timbre :as timbre])
+  (:import (com.fasterxml.jackson.databind SerializationFeature)))
+
+(def object-mapper (doto (json/object-mapper)
+                     (.configure SerializationFeature/FAIL_ON_EMPTY_BEANS false)))
+
+(defn json-appender
+  "Creates Timbre configuration map for JSON appender"
+  []
+  {:enabled? true
+   :async? false
+   :min-level nil
+   :fn (fn [{:keys [instant level ?ns-str ?file ?line ?err vargs]}]
+         (println (json/write-value-as-string (cond-> {:timestamp instant
+                                                       :level level
+                                                       :thread (.getName (Thread/currentThread))}
+                                                ?err (->
+                                                      (assoc :err (Throwable->map ?err))
+                                                      (assoc :ns ?ns-str)
+                                                      (assoc :file ?file)
+                                                      (assoc :line ?line))
+                                                (even? (count vargs)) (assoc :args (apply hash-map vargs))
+                                                (odd? (count vargs)) (->
+                                                                      (assoc :msg (first vargs))
+                                                                      (assoc :args (apply hash-map (rest vargs)))))
+                                              object-mapper)))})
+
+(defn install
+  "Installs json-appender as the sole appender for Timbre"
+  ([]
+   (install :info))
+  ([level]
+   (timbre/set-config! {:level level
+                        :appenders {:json (json-appender)}})))
+
+(defn log-success [request-method uri status]
+  (timbre/info :method request-method :uri uri :status status))
+
+(defn log-failure [t request-method uri]
+  (timbre/error t "Failed to handle request" :method request-method :uri uri))
+
+(defn wrap-json-logging
+  "Ring middleware for JSON logging. Logs :method, :uri and :status for successfull handler invocations,
+  :method and :uri for failed invocations."
+  [handler]
+  (fn
+    ([{:keys [request-method  uri] :as request}]
+     (try
+       (let [{:keys [status] :as response} (handler request)]
+         (log-success request-method uri status)
+         response)
+       (catch Throwable t
+         (log-failure t request-method uri)
+         {:status 500
+          :body "Server error"})))
+    ([{:keys [request-method  uri] :as request} respond raise]
+     (handler request
+              (fn [{:keys [status] :as response}]
+                (log-success request-method uri status)
+                (respond response))
+              (fn [t]
+                (log-failure t request-method uri)
+                (raise t))))))
---
+++ test/timbre_json_appender/core_test.clj
@@ -0,0 +1,42 @@
+(ns timbre-json-appender.core-test
+  (:require [clojure.test :refer [deftest is testing] :as t]
+            [timbre-json-appender.core :as sut]
+            [jsonista.core :as json]
+            [taoensso.timbre :as timbre]))
+
+(timbre/set-config! {:level :info
+                     :appenders {:json (sut/json-appender)}})
+
+(def object-mapper (json/object-mapper {:decode-key-fn true}))
+
+(defn parse-string [str]
+  (json/read-value str object-mapper))
+
+(deftest only-message
+  (is (= "Hello" (:msg (parse-string (with-out-str (timbre/info "Hello")))))))
+
+(deftest only-args
+  (let [log (parse-string (with-out-str (timbre/info :status 200 :duration 5)))]
+    (is (= 200 (-> log :args :status)))
+    (is (= 5 (-> log :args :duration)))))
+
+(deftest message-and-args
+  (let [log (parse-string (with-out-str (timbre/info "Task done" :duration 5)))]
+    (is (= "Task done" (:msg log)))
+    (is (= 5 (-> log :args :duration)))))
+
+(deftest unserializable-value
+  (testing "in a field"
+    (is (= {} (-> (parse-string (with-out-str (timbre/info :a (Object.))))
+                  :args
+                  :a))))
+  (testing "in ExceptionInfo"
+    (is (= {} (-> (parse-string (with-out-str (timbre/info (ex-info "poks" {:a (Object.)}))))
+                  :err
+                  :data
+                  :a)))))
+
+(deftest exception
+  (is (= "poks" (-> (parse-string (with-out-str (timbre/info (Exception. "poks") "Error")))
+                    :err
+                    :cause))))
