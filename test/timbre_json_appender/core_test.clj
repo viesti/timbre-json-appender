f74ea66e9b296ab95f54979513cd78580ae246ff
R.A. Porter <richard.porter@rate.com>
2021-10-03 10:53:40 -0700

    Adds pre-processor functionality for ex-info data map fields

--- test/timbre_json_appender/core_test.clj
+++ test/timbre_json_appender/core_test.clj
@@ -154,6 +154,21 @@
                                     (timbre/info :a 1)))))]
         (is (= 123 (:test-context log)))))))
 
+(deftest data-field-fn
+  (testing "default function passes all values as they are"
+    (let [log (parse-string (with-out-str
+                              (timbre/with-config {:level :info
+                                                   :appenders {:json (sut/json-appender)}}
+                                (timbre/info (ex-info "plop" {:user-id 1 :name "alice"})))))]
+      (is (= {:user-id 1 :name "alice"} (get-in log [:err :data])))))
+
+  (testing "provided function processes values in data map"
+    (let [log (parse-string (with-out-str
+                              (timbre/with-config {:level :info
+                                                   :appenders {:json (sut/json-appender {:data-field-fn #(when (number? %) %)})}}
+                                (timbre/info (ex-info "plop" {:user-id 1 :name "alice"})))))]
+      (is (= {:user-id 1 :name nil} (get-in log [:err :data]))))))
+
 (deftest should-log-field-fn
   (testing "default function omits fields on non-errors"
     (let [log (parse-string (with-out-str
