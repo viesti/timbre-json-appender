9c4328d23d30968181141ed102305c0b33ff5813
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2022-02-01 08:28:04 +0200

    Add helper to test with Leiningen projects locally

---
+++ bin/local-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# For testing with Leiningen/maven projects, which don't handle gitlibs
+
+if [ -z $1 ]; then
+    echo "usage: $0 <version>"
+    exit 1
+fi
+
+VERSION=$1
+GIT_HASH=$(git rev-parse HEAD)
+
+set -eux
+
+mkdir -p target
+
+cp pom-template.xml pom.xml
+clojure -Srepro -Spom
+sed -e "s/VERSION/$VERSION/g" \
+    -e "s/GIT_HASH/$GIT_HASH/g" \
+    -i '' \
+    pom.xml
+mv pom.xml target/pom.xml
+
+clojure -Srepro \
+        -A:pack \
+        mach.pack.alpha.skinny \
+        --no-libs \
+        --project-path target/timbre-json-appender.jar
+mvn install:install-file \
+    -Dfile=target/timbre-json-appender.jar \
+    -DrepositoryId=clojars \
+    -Durl=https://clojars.org/repo \
+    -DpomFile=target/pom.xml
